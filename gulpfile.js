be4f01719b62b0c3a49aa9c5fded24619024db87
wailmadrane <wail.madrane@gmail.com>
2019-08-29 22:18:57 +0200

    adding clear function

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,40 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+const del = require('del');
 
-function style() {
-   return gulp.src('./src/scss/**/*.{scss,sass}')
+const style = () => {
+   return gulp.src('./src/scss/**/*.{scss,sass}', {sourcemaps: true})
       .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('./dist/css/'))
+      .pipe(gulp.dest('./dist/css/', {sourcemaps: '.'}))
       .pipe(browserSync.stream());
 }
 
-function html() {
+const html = () => {
    return gulp.src('./src/*.html')
       .pipe(gulp.dest('./dist/'))
       .pipe(browserSync.stream());
 }
 
-function watch() {
+const clear = () => {
+   return del('./dist');
+}
+
+const watch = () => {
    browserSync.init({
       server: {
           baseDir: "./dist/"
       }
-  });
-  gulp.watch('./src/scss/**/*.{scss,sass}', style);
-  gulp.watch('./src/*.html', html);
-  gulp.watch(['./dist/css/*.css', './dist/*.html']).on('change', browserSync.reload);
+   });
+   gulp.watch('./src/scss/**/*.{scss,sass}', style);
+   gulp.watch('./src/*.html', html);
+   gulp.watch(['./dist/css/*.css', './dist/*.html']).on('change', browserSync.reload);
 }
 
 module.exports = {
    style,
    html,
+   clear,
    watch,
-   default: gulp.series(style, html, watch)
+   default: gulp.series( gulp.parallel(style, html), watch)
 }
